9573d3e4fc9bf7ee1046ad716a3baac746f1fda9
Álvaro Braz <alvarobraz83@gmail.com>
2025-10-23 16:26:07 -0300

    feat: create components

--- src/App.tsx
+++ src/App.tsx
@@ -15,56 +15,64 @@ import Button from "./components/button";
 import InputText from "./components/input-text";
 import SelectTime from "./components/SelectTime";
 import SelectDate from "./components/SelectDate";
+import Card from "./components/card";
+import Container from "./components/container";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
-    <div className="flex flex-col gap-2">
-      <Text variant="title-lg" className="text-pink-base">Olá mundo!</Text>
-      <Text className="text-green-base">Olá mundo!</Text>
-      <Text variant="title-md">Olá mundo!</Text>
-      <Text variant="title-sm">Olá mundo!</Text>
-      <Text variant="text-md">Olá mundo!</Text>
-      <Text variant="text-sm">Olá mundo!</Text>
-      <Text>Levar o dog pra passear</Text>
-    </div>
-    <div className="flex gap-1">
-      <Icon svg={Calendar} className="fill-yellow" />
-      <Icon svg={CaretDown} className="fill-yellow" />
-      <Icon svg={CaretLeft} className="fill-yellow" />
-      <Icon svg={CaretRight} className="fill-yellow" />
-      <Icon svg={cloudSun} className="fill-yellow" />
-      <Icon svg={MoonStars} className="fill-yellow" />
-      <Icon svg={SunHorizon} className="fill-yellow" />
-      <Icon svg={TrashIcon} className="fill-yellow" />
-      <Icon svg={UserSquare} className="fill-yellow" />
-    </div>
-    <div className=" gap-1">
-        <Button>AGENDAR</Button>
-        <Button disabled>AGENDAR</Button>
-      </div>
-    <div className="flex gap-1">
-      <ButtonIcon icon={TrashIcon} />
-    </div>
-    <div>
-      <InputText
-        icon={UserSquare}
-        iconVariant="primary"
-        placeholder="Nome do cliente"
-        size="md"
-      />
-    </div>
-    <SelectTime
-      disabledTimes={["08:00", "09:30"]}
-      onSelect={(time) => console.log(`Horário selecionado: ${time}`)}
-    />
+    <Container>
+      <div className="grid gap-3">
+        <div className="flex flex-col gap-2">
+          <Text variant="title-lg" className="text-pink-base">Olá mundo!</Text>
+          <Text className="text-green-base">Olá mundo!</Text>
+          <Text variant="title-md">Olá mundo!</Text>
+          <Text variant="title-sm">Olá mundo!</Text>
+          <Text variant="text-md">Olá mundo!</Text>
+          <Text variant="text-sm">Olá mundo!</Text>
+          <Text>Levar o dog pra passear</Text>
+        </div>
+        <div className="flex gap-1">
+          <Icon svg={Calendar} className="fill-yellow" />
+          <Icon svg={CaretDown} className="fill-yellow" />
+          <Icon svg={CaretLeft} className="fill-yellow" />
+          <Icon svg={CaretRight} className="fill-yellow" />
+          <Icon svg={cloudSun} className="fill-yellow" />
+          <Icon svg={MoonStars} className="fill-yellow" />
+          <Icon svg={SunHorizon} className="fill-yellow" />
+          <Icon svg={TrashIcon} className="fill-yellow" />
+          <Icon svg={UserSquare} className="fill-yellow" />
+        </div>
+        <div className=" gap-1">
+            <Button>AGENDAR</Button>
+            <Button disabled>AGENDAR</Button>
+          </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+        </div>
+        <div>
+          <InputText
+            icon={UserSquare}
+            iconVariant="primary"
+            placeholder="Nome do cliente"
+            size="md"
+          />
+        </div>
+        <SelectTime
+          disabledTimes={["08:00", "09:30"]}
+          onSelect={(time) => console.log(`Horário selecionado: ${time}`)}
+        />
 
-    <SelectDate
-        icon={Calendar}
-        iconVariant="primary"
-        onSelect={(date) => console.log(`Data selecionada: ${date}`)}
-      />
-    </div>
-    
+        <SelectDate
+            icon={Calendar}
+            iconVariant="primary"
+            onSelect={(date) => console.log(`Data selecionada: ${date}`)}
+          />
+        <div>
+          <div>
+            <Card size="md">Olá mundo</Card>
+          </div>
+        </div>
+      </div>
+    </Container>
   )
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,49 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+ 
+export const cardVariants = cva(
+  `
+    
+  `,
+  {
+    variants: {
+      variant: {
+        primary: "bg-[color:var(--color-gray-700)]",
+        secondary: "bg-transparent"
+      },
+      size: {
+        none: "",
+        md: "p-20",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "none",
+    },
+  }
+);
+
+interface CardProps
+  extends VariantProps<typeof cardVariants>,
+    React.ComponentProps<"div"> {
+  as?: keyof React.JSX.IntrinsicElements;
+}
+
+export default function Card({
+  as = "div",
+  size,
+  variant,
+  children,
+  className,
+  ...props
+}: CardProps) {
+  return React.createElement(
+    as,
+    {
+      className: cardVariants({ size, variant, className }),
+      ...props,
+    },
+    children
+  );
+}
